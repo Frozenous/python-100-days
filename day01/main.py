51ae1f41e432b9b9864df31de0ffc0336b748810
Brian Bartow <brian.bartow23@gmail.com>
2026-01-28 12:00:41 -0800

    day01

--- day01/main.py
+++ day01/main.py
@@ -1,1 +1,4 @@
-﻿print('Day 1 ready')
+﻿print("Welcome to Band Name Generator.")
+city = input("Pick a city: \n")
+pet_name = input("Pick a pet name: \n")
+print("Your Band Name is: " + city + " " + pet_name)
